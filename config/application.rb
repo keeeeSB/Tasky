cffa1516537c8a303f14a5b6d44fae26f182236f
KeeeeSB <kfp.a@icloud.com>
2026-01-13 09:23:56 +0900

    時間などの設定

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,19 @@ module Tasky
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       model_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.skip_routes true
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
