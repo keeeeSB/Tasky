d76f39ebed49b30f1f1203be1a43f871dc70b2d4
KeeeeSB <kfp.a@icloud.com>
2026-01-17 08:28:57 +0900

    viewsの追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
