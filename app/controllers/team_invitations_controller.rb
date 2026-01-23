82ef9afe15e50ded973187bfd429048c63b8bb2d
KeeeeSB <kfp.a@icloud.com>
2026-01-23 10:32:48 +0900

    TeamINvitationコントローラーにアクションを追加

---
+++ app/controllers/team_invitations_controller.rb
@@ -0,0 +1,21 @@
+class TeamInvitationsController < ApplicationController
+  def accept
+    invitation = TeamInvitation.find_by(token: params[:token])
+
+    if invitation.nil? || invitation.expires_at < Time.current
+      redirect_to root_path, alert: '無効なリンクです。'
+      return
+    end
+
+    if user_signed_in?
+      ActiveRecord::Base.transaction do
+        current_user.update!(team: invitation.team)
+        invitation.destroy!
+      end
+      redirect_to root_path, notice: 'チームに参加しました。'
+    else
+      session[:team_invitation.token] = invitation.token
+      redirect_to new_user_session_path, notice: 'ログインしてください。'
+    end
+  end
+end
